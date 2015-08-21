daf69db3031f3bdd1fb073c3e7574608acc1ad66
Korben Zhang <korbenzhang@gmail.com>
2015-08-21 13:47:01 +0800

    add task hello

---
+++ task_hello/hello.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main(int argc, char *argv[])
+{
+	std::cout << "C++: Hello, World!" << std::endl;
+	return 0;
+}
---
+++ tasks.md
@@ -0,0 +1,9 @@
+Tasks
+=====
+
+
+Hello, World
+------------
+The basic Example for all kinds of languages.
+
+
